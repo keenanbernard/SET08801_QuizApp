c3fbc281f1b4e63510e637046de85bb278971165
Keenan Bernard <k.bernard@livedigi.com>
2022-04-28 20:11:03 -0600

    Update of Media Query

--- appFunctions.js
+++ appFunctions.js
@@ -1,6 +1,6 @@
 //selecting all required elements through classes
 const quizBody = document.body;
-const toggleButton = document.querySelector(".toggleButton button");
+const toggleMode = document.getElementById('tB');
 const homeButton = document.querySelector(".hmeButton button");
 const vlmButton = document.querySelector(".vlmButton button");
 const startButton = document.querySelector(".strtButton button");
@@ -15,9 +15,9 @@ const timerLine = document.querySelector("header .timer_line");
 const resultsBox = document.querySelector(".resultsBox");
 const quizSong = document.getElementById("quizSong");
 
-const chk = document.getElementById('chk');
 
-chk.addEventListener('change', () => {
+// action if button is selected to enable dark mode
+toggleMode.addEventListener('change', () => {
     quizBody.classList.toggle("dark-mode");
     homeButton.classList.toggle("dark-mode");
     vlmButton.classList.toggle("dark-mode");
